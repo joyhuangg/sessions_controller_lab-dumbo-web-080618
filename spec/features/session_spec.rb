2b19dece809d679d1c6725d89053559140c796d4
joyhuangg <joyhuangg@gmail.com>
2018-09-09 14:30:21 -0400

    Done.

---
+++ spec/features/session_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "homepage", :type => :view do
+  # it "redirects to login page if user not logged in" do
+  #   visit '/'
+  #   expect(response).to redirect_to controller: 'sessions', action: 'new'
+  # end
+  # it "redirects to hello page if user is logged in", :type => :view do
+  #   visit '/'
+  #   expect(response).to redirect_to controller: 'application', action: 'hello'
+  # end
+end
